c9e7da720398ba602155756bf29012e68dc5eacc
Nathan Seifert <naseifer@ualberta.ca>
2015-10-15 16:10:06 -0400

    Working SQL reader test

--- prototyping/mysql_test.py
+++ prototyping/mysql_test.py
@@ -7,12 +7,33 @@ import pandas as pd
 def rd_pass():
     return open('pass.pass').read()
 
-def parse_table_main(res):
-    i = 0
-    print ""
+
+def get_main_cols():
+    cols_dict = {}
+    list = open("main_columns.csv").read().split('\n')
+    for entry in list:
+        try:
+            splitted = entry.strip().split(',')
+            cols_dict[splitted[0]] = splitted[1]
+        except IndexError:
+            continue
+
+    return cols_dict
+
+
+def parse_table_main(res, cols=get_main_cols()):
+
+    struct = {}
+
+    for col_key in cols:
+        struct[cols[col_key]] = pd.Series([row[int(col_key)] for row in res])
+
+    column_keys = sorted([int(key) for key in cols.keys()])
+
+    return pd.DataFrame(struct, columns=[cols[str(i)] for i in column_keys])
+
 
 if __name__ == "__main__":
-
     HOST = "sql.cv.nrao.edu"
     LOGIN = "nseifert"
     PASS = rd_pass()
@@ -30,7 +51,8 @@ if __name__ == "__main__":
 
     cursor.execute(cmd)
 
-    parse_table_main(cursor.fetchall())
+    listing = parse_table_main(cursor.fetchall())
 
+    listing.to_excel('test.xlsx', sheet_name="Output test")
 
     db.close()
