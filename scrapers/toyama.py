ad5a16f1f263dad0625f88252038a0fb0659afb0
Nathan Seifert <naseifer@ualberta.ca>
2016-10-31 14:39:53 -0600

    Huge additions to Toyama. Still needs a bit of work to database pushing for appending new data to an existing species.

--- scrapers/toyama.py
+++ scrapers/toyama.py
@@ -5,18 +5,65 @@ import numpy as np
 import re
 import time
 import MySQLdb as sqldb
-
+import easygui as eg
+from os.path import exists
 
 def format_qns(df, fmt):
-
-    fmt_idx_up = fmt['index_up']
-    fmt_idx_dwn = fmt['index_down']
+    # Resolve indicies
+    fmt_idx = fmt['index']
     fmt_style = fmt['style']
 
-    df['resolved_QNs'] = df[['Uqn%s'%i for i in fmt_idx_up]+['Lqn%s'%i for i in fmt_idx_dwn]].apply(lambda x: fmt_style.format(*x), axis=1)
+    if len(fmt_idx) % 2 == 1 or len(fmt_idx) == 0:
+
+        print 'You shouldn\'t have ever come here.'
+        return 0
+
+    else:
+
+        order = []
+        for val in fmt_idx:
+
+            try:
+
+                if 'up' in val:
+                    order.append('Uqn%s' %[int(s) for s in val if s.isdigit()][0])
+                elif 'low' in val:
+                    order.append('Lqn%s' %[int(s) for s in val if s.isdigit()][0])
+            except IndexError:
+                print 'Your QN# input is malformed in some way. It failed on: %s ' %val
+                raise
+    df['resolved_QNs'] = df[order].apply(lambda x: fmt_style.format(*x), axis=1)
+
     if 'Symmetry' in df.columns.values:
         df['resolved_QNs'] = df['resolved_QNs'] + ' ' + df['Symmetry']
 
+    # Now let's do a quick regex to check if there are any QNs of the form "N_IJ" and replace with N(I, J)
+    def fix_QNs(buffer, regex='(\\d+_\\d+)'):
+        out = buffer
+        r = re.compile(regex)
+        res = re.findall(r, buffer)
+        if res:
+            for val in res:
+
+                upper_qn, lower_qn = val.split('_')
+                if len(lower_qn) >= 3:
+                    if len(upper_qn) % 2 == 0:
+                        idx = len(lower_qn)/2
+                        out = out.replace(val, upper_qn+'(%s, %s)' %(lower_qn[:idx], lower_qn[idx:]))
+                    else:
+                        # e.g. Ka,Kc = 2,10 or 10,2, etc.
+                        idx = len(lower_qn)/2 - 1
+                        if int(lower_qn[:idx]) + int(lower_qn[idx:]) in [int(upper_qn)+1, int(upper_qn)]:
+                            out = out.replace(val, upper_qn+'(%s, %s)' %(lower_qn[:idx], lower_qn[idx:]))
+                        else:
+                            out = out.replace(val, upper_qn+'(%s, %s)' %(lower_qn[:idx+1], lower_qn[idx+1:]))
+
+                else:
+                    out = out.replace(val, upper_qn+'(%s, %s)' %(lower_qn[:1], lower_qn[1:])) # Ka, Kc < 10
+
+        return out
+
+    df['resolved_QNs'] = df['resolved_QNs'].apply(fix_QNs)
     return df
 
 
@@ -61,8 +108,46 @@ def parse_html(inp):
 
     data = pd.DataFrame(tbl_data)
     data.columns = tbl_headers
-    return data
 
+    # Get rid of any columns that are all NaN entries
+    return data.dropna(axis=1, how='all')
+
+def inp_metadata(meta_inp_path):
+    mdata = {}
+
+    # Dictionaries to connect string values in JPL metadata to SQL columns
+    tags = {'TITLE': 'Name', 'CHEMICAL_NAME': 'chemical_name', 'SHORT_NAME': 's_name',
+            'Q(300.0)': 'Q_300_0', 'Q(225.0)': 'Q_225_0', 'Q(150.0)': 'Q_150_0',
+            'Q(75.00)': 'Q_75_00', 'Q(37.50)': 'Q_37_50', 'Q(18.75)': 'Q_18_75', 'Q(9.375)': 'Q_9_375',
+            'A': 'A', 'B': 'B', 'C': 'C', 'MU_A': 'MU_A',  'MU_B': 'MU_B', 'MU_C': 'MU_C',
+            'CONTRIBUTOR': 'Contributor', 'REF': 'Ref1'}
+
+
+    fill_ins = {'v3_0': 3, 'LineList': 16}
+
+    for line in open(meta_inp_path, 'r').read().split('\n'):
+        if line[0] == '!':
+            continue
+
+        id, value = line.split(':')
+        try:
+            mdata[tags[id]] = value.strip()
+        except KeyError:
+            if 'ref' in id.lower():
+                i = 1
+                for j in range(i, 21):
+                    if 'Ref%i'%j not in mdata.keys():
+                        break
+                    i += 1
+                mdata['Ref%i'%i] = value.strip()
+            else:
+                continue
+
+    mdata['Date'] = time.strftime('%b %Y', time.gmtime())
+    for key in fill_ins.keys():
+        mdata[key] = fill_ins[key]
+
+    return mdata
 
 def prep_data(df, opt):
 
@@ -74,8 +159,8 @@ def prep_data(df, opt):
     for col in set(df.columns.values)-set(rename_dict.values()):
         df.drop(col, axis=1, inplace=True)
 
-    df[['measfreq', 'measerrfreq', 'lower_state_energy', 'intintensity']] = \
-        df[['measfreq', 'measerrfreq', 'lower_state_energy', 'intintensity']].apply(pd.to_numeric)
+    df[['measfreq', 'measerrfreq']] = \
+        df[['measfreq', 'measerrfreq']].apply(pd.to_numeric)
 
     df['error'] = df['measerrfreq']
     df['roundedfreq'] = df['measfreq'].round(decimals=0)
@@ -83,12 +168,15 @@ def prep_data(df, opt):
     df['line_wavelength'] = 299792458./(df['measfreq']*1.0E6)*1000
 
     if 'lower_state_energy' in df.columns.values:
+        pd.to_numeric(['lower_state_energy'], errors='coerce')
         df['lower_state_energy_K'] = 1.438786296 * df['lower_state_energy']
         df['upper_state_energy'] = df['lower_state_energy'] + df['measfreq']/29979.2458
         df['upper_state_energy_K'] = 1.438786296 * df['upper_state_energy']
 
-    df['intintensity'] = df['intintensity']/max(df['intintensity'])
-    df['intintensity'].apply(lambda x: np.log10(x))
+    if 'intintensity' in df.columns.values:
+        df['intintensity'] = pd.to_numeric(df['intintensity'], errors='coerce')
+        df['intintensity'] = df['intintensity']/max(df['intintensity'])
+        df['intintensity'].apply(lambda x: np.log10(x))
 
     df['ll_id'] = 16
     df['`v3.0`'] = 3
@@ -168,64 +256,106 @@ def push_data(df, meta, new_species=False, species_data=None):
 
     cursor.close()
 
+def main(db):
+    pd.options.mode.chained_assignment = None # default='warn'
 
-if __name__ == "__main__":
+    ToyamaLoop = True
 
-    opt = {
-    'path': '/media/sf_Share/toyama/data/',
-    'file': 'C2H5OCH3_v29.html',
+    while ToyamaLoop:
 
-    'qn_style': '{}({}, {}) - {}({}, {})',
-    'qn_index_up': [2, 3, 4],
-    'qn_index_down': [2, 3, 4],
+        opt = {}
 
-    'ism_molecule': False,
-    }
+        opt['path'] = eg.fileopenbox(msg='Please select input HTML table for Toyama entry', title='Toyama table entry')
 
-    meta = {
-        'species_id': 21163,
-        'LineList': 16,
-        'v3_0': 3,
-        'Name': 'Ethyl Methyl Ether, v<sub>28</sub> = 1',
-        'Date': time.strftime('%b. %Y'),
-        'Contributor': 'Nathan Seifert',
+        qn_style_settings = {
+            'msg': 'Please specify your quantum number style and ordering. \n'
+                   'Please see README.md for specific examples of QN style formatters.',
+            'title': 'QN Style Entry for Toyama',
+            'fields': ['QN Style', 'QN Index Ordering']
+        }
+        qn_style = eg.multenterbox(msg=qn_style_settings['msg'],
+                                   title=qn_style_settings['title'], fields=qn_style_settings['fields'])
 
-        'Ref1': open(opt['path']+'C2H5OCH3_ref.txt', 'r').read(),
-        'Ref2': 'CONTRIBUTOR NOTE: Intensities are sourced from the "Intensity" column of the ToyaMA entry, but'
-                'they are presented here in normalized and logarithmic form.',
+        opt['qn_style'] = qn_style[0]
+        opt['qn_index'] = [x.strip().lower() for x in qn_style[1].strip().split(',')]
 
-        'A': '',
-        'B': '',
-        'C': '',
-        'MU_A': '',
-        'MU_B': '',
-        'MU_C': ''
-    }
+        NewOrAppend = eg.buttonbox(msg='Do you want to add this entry as a new molecule or append to previously added Splat species?',
+                                   title='New or append?', choices=['New', 'Append', 'QUIT'])
+        if NewOrAppend == 'New':
 
-    species = {
-        'species_id': meta['species_id'],
-        'name': 'C<sub>2</sub>H<sub>5</sub>OCH<sub>3</sub>, v<sub>28</sub> = 1',
-        'chemical_name': 'Ethyl Methyl Ether',
-        's_name': 'C2H5OCH3, v28=1',
-        's_name_noparens': 'C2H5OCH3, v28=1',
-        'SPLAT_ID': '06028',
+            species = {}
 
-        'potential': 1,
-        'probable': 0,
-        'known_ast_molecules': 0,
+            spec_inp_menu = {'msg': 'Please enter species entry information.',
+                             'title': 'Species entry menu.',
+                             'fields': ['name', 'chemical_name', 's_name', 's_name_noparens', 'SPLAT_ID',
+                                        'potential', 'probable', 'known_ast_molecules']}
 
-        # Shouldn't have to change the ones below
-        'created': 'NOW()',
-        'atmos': 0,
-        'resolved': 1,
-        'Top20': 0,
-        'version': 1,
-    }
+            species_inp = eg.multenterbox(msg=spec_inp_menu['msg'], title=spec_inp_menu['title'], fields=spec_inp_menu['fields'])
 
+            if species_inp:
 
-    input_table = open(opt['path']+opt['file'],'r')
-    print 'Loaded file from path: %s' %opt['path']+opt['file']
+                for i, val in enumerate(spec_inp_menu['fields']):
+                    species[val] = species_inp[i]
 
-    data = format_qns(parse_html(input_table), fmt={'style': opt['qn_style'], 'index_up': opt['qn_index_up'], 'index_down': opt['qn_index_down']})
-    data = prep_data(data, opt)
-    push_data(data, meta, new_species=True, species_data=species)
+            else:
+                break
+
+            # Generate new species ID
+            cur = db.cursor()
+            cur.execute('SELECT MAX(species_id) FROM species')
+            species['species_id'] = str(int(cur.fetchall()[0][0])+1)
+
+        elif NewOrAppend == 'Append':
+            # We at least need the species ID in order to push
+            splat_id = eg.enterbox(msg='Please enter the SPLAT ID # of the species you wish to append to.')
+
+            cur = db.cursor()
+            cur.execute('SELECT species_id from species where SPLAT_ID = %s' %splat_id)
+            res = cur.fetchall()
+            cur.close()
+
+            if res:
+                species = res[0][0]
+            else:
+                break
+
+        else:
+            break
+
+        # Generate metadata
+        MetadataInpChoice = eg.buttonbox(msg='Do you want to supply a metadata input file?', title='Metadata input', choices=['Input File', 'Manual Entry'])
+
+        if MetadataInpChoice:
+            if MetadataInpChoice == 'Input File':
+                meta = inp_metadata(eg.fileopenbox(msg='Please choose a valid metadata input file.', title='Metadata input'))
+            elif MetadataInpChoice == 'Manual Entry':
+                meta = {}
+
+                MetaFields = ['Name', 'Contributor', 'Ref1', 'Ref2', 'A', 'B', 'C', 'MU_A', 'MU_B', 'MU_C']
+                MetaChoices = eg.multenterbox(msg='Please enter basic metadata information.', title='Metadata entry', fields=MetaFields)
+                for i, val in enumerate(MetaFields):
+                    # Check if meta entry is a text file -- for Ref only
+                    if 'Ref' in val and exists(MetaChoices[i]):
+                        try:
+                            meta[val] = open(MetaChoices[i], 'r').read()
+                        except:
+                            meta[val] = ''
+                        continue
+
+                    meta[val] = MetaChoices[i]
+
+            if isinstance(species, dict):
+                meta['species_id'] = species['species_id']
+            else:
+                meta['species_id'] = species
+
+        else:
+            break
+
+        if NewOrAppend == 'New':
+            data = format_qns(parse_html(open(opt['path'],'r')), fmt={'style': opt['qn_style'], 'index': opt['qn_index']})
+            push_data(prep_data(data, opt), meta, new_species=True, species_data=species)
+
+        else:
+            data = format_qns(parse_html(open(opt['path'],'r')), fmt={'style': opt['qn_style'], 'index': opt['qn_index']})
+            push_data(prep_data(data, opt), meta, new_species=False)
